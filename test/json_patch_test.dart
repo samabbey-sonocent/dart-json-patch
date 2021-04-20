9221651c0fabb6522f67a934f6bde1b602ca627e
samabbey-sonocent <61702678+samabbey-sonocent@users.noreply.github.com>
2021-04-20 13:19:27 +0100

    Revert "Removing the error when adding at an existing location"
    This reverts commit 5fbd5eb307db22ce181b1638e768c63b45d3fe77.

--- test/json_patch_test.dart
+++ test/json_patch_test.dart
@@ -199,6 +199,13 @@ void main() {
     test('.apply throws at invalid operations', () {
       expect(
           () => JsonPatch.apply({
+                'a': 5
+              }, [
+                {'op': 'add', 'path': '/a', 'value': 6}
+              ]),
+          throwsA(anything));
+      expect(
+          () => JsonPatch.apply({
                 'test': 5
               }, [
                 {'op': 'replace', 'path': '/a', 'value': 6}
